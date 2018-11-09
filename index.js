67df8ea8272af71e298b526e3aa317dcf496a01c
Sergio Neves <serneves@hotmail.com>
2018-11-09 11:40:28 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,39 @@
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  newKittens = [...kittens, name]
+  return newKittens
+}
+
+function prependKitten(name) {
+  newKittens = [name, ...kittens]
+  return newKittens
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, lenght -1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
